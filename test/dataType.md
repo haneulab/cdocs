78c8167f6393b9357b2655bb70e9c747bf54efb7
haneulab <hanchoi@ucdavis.edu>
2022-06-03 20:12:15 -0700

    created dataType file in ./test (master)

---
+++ test/dataType.md
@@ -0,0 +1,22 @@
+# Data Type C
+
+Basic explanation of C language data types
+
+- int
+- float
+- ....
+
+### How to declare integers in C
+
+Please look at the following example
+
+```c
+int main()
+{
+    /*
+    dsadsadas
+    */
+    int num = 0;
+    float flt = 0.5;
+}
+```
---
+++ test/pointer.c
@@ -0,0 +1,17 @@
+/**
+ * @file pointer.c
+ * @author Haneul Choi (hanchoi@ucdavis.edu)
+ * @brief showing example of pointer
+ * @version 0.1
+ * @date 2022-06-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("hello\n");
+    return 0;
+}
